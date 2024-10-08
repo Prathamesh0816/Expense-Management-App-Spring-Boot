2dbb3e70101a18b2d689d4476c04747ddade155e
Prathamesh Katikar <pgkatikar@gmail.com>
2024-10-08 12:35:52 +0530

    initial commit

---
+++ expensemgmtapp_v1/src/test/java/com/itshaala/expensemgmtapp_v1/ExpensemgmtappV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itshaala.expensemgmtapp_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensemgmtappV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
